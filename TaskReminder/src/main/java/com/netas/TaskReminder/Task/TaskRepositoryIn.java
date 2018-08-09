89768438886d5b4b585baa6dceb356d554884643
Burak Kağan Korkmaz <bkorkmaz@gyte.edu.tr>
2018-08-09 16:39:24 +0300

    TaskReminder Project created
    The Project Module has created in general.

---
+++ TaskReminder/src/main/java/com/netas/TaskReminder/Task/TaskRepositoryIn.java
@@ -0,0 +1,6 @@
+package com.netas.TaskReminder.Task;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TaskRepositoryIn extends CrudRepository<Task, Integer> {
+}
---
+++ TaskReminder/src/main/java/com/netas/TaskReminder/Task/TaskService.java
@@ -0,0 +1,42 @@
+package com.netas.TaskReminder.Task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks(){
+        return (List<Task>) taskRepository.findAll();
+    }
+
+    public Task getTaskById(Integer id){
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    public void addTask(Task task){
+        taskRepository.save(task);
+    }
+
+    public void updateTask(Task task){
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Task task){
+        taskRepository.delete(task);
+    }
+
+    public void deleteTaskById (Integer id){
+        taskRepository.deleteById(id);
+    }
+
+    public void deleteAllTask(){
+        taskRepository.deleteAll();
+    }
+
+}
