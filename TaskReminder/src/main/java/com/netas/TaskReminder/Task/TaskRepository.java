f0320a097908a29afb6609a7d9abe7750e4629e0
Burak Kağan Korkmaz <bkorkmaz@gyte.edu.tr>
2018-08-13 17:03:17 +0300

    Code Reformatting

--- TaskReminder/src/main/java/com/netas/TaskReminder/Task/TaskRepository.java
+++ TaskReminder/src/main/java/com/netas/TaskReminder/Task/TaskRepository.java
@@ -3,58 +3,59 @@ package com.netas.TaskReminder.Task;
 import java.util.Optional;
 
 public class TaskRepository implements TaskRepositoryIn {
-    @Override
-    public <S extends Task> S save(S s) {
-        return null;
-    }
 
-    @Override
-    public <S extends Task> Iterable<S> saveAll(Iterable<S> it) {
-        return null;
-    }
+  @Override
+  public <S extends Task> S save(S s) {
+    return null;
+  }
 
-    @Override
-    public Optional<Task> findById(Integer id) {
-        return Optional.empty();
-    }
+  @Override
+  public <S extends Task> Iterable<S> saveAll(Iterable<S> it) {
+    return null;
+  }
 
-    @Override
-    public boolean existsById(Integer id) {
-        return false;
-    }
+  @Override
+  public Optional<Task> findById(Integer id) {
+    return Optional.empty();
+  }
 
-    @Override
-    public Iterable<Task> findAll() {
-        return null;
-    }
+  @Override
+  public boolean existsById(Integer id) {
+    return false;
+  }
 
-    @Override
-    public Iterable<Task> findAllById(Iterable<Integer> it) {
-        return null;
-    }
+  @Override
+  public Iterable<Task> findAll() {
+    return null;
+  }
 
-    @Override
-    public long count() {
-        return 0;
-    }
+  @Override
+  public Iterable<Task> findAllById(Iterable<Integer> it) {
+    return null;
+  }
 
-    @Override
-    public void deleteById(Integer id) {
+  @Override
+  public long count() {
+    return 0;
+  }
 
-    }
+  @Override
+  public void deleteById(Integer id) {
 
-    @Override
-    public void delete(Task task) {
+  }
 
-    }
+  @Override
+  public void delete(Task task) {
 
-    @Override
-    public void deleteAll(Iterable<? extends Task> it) {
+  }
 
-    }
+  @Override
+  public void deleteAll(Iterable<? extends Task> it) {
 
-    @Override
-    public void deleteAll() {
+  }
 
-    }
+  @Override
+  public void deleteAll() {
+
+  }
 }
--- TaskReminder/src/main/java/com/netas/TaskReminder/Task/TaskRepositoryIn.java
+++ TaskReminder/src/main/java/com/netas/TaskReminder/Task/TaskRepositoryIn.java
@@ -3,4 +3,5 @@ package com.netas.TaskReminder.Task;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TaskRepositoryIn extends CrudRepository<Task, Integer> {
+
 }
