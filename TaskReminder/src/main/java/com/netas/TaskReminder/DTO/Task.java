8297fc87693d7083aaa8c19e1045e688ea42ed8b
Unknown <hayriye.96.44@gmail.com>
2018-08-16 11:44:32 +0300

    Many-to-Many  Relationship

--- TaskReminder/src/main/java/com/netas/TaskReminder/DTO/Task.java
+++ TaskReminder/src/main/java/com/netas/TaskReminder/DTO/Task.java
@@ -1,92 +1,72 @@
 package com.netas.TaskReminder.DTO;
-
+import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "task")
-public class Task {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Integer id;
-  @Column(name = "summary")
-  private String summary;
-  @Column(name = "description")
-  private String description;
-  @Column(name = "is_completed")
-  private boolean isCompleted;
-  @Column(name = "createTime")
-  private Date createTime;
-  @Column(name = "duration")
-  private int duration;
-
-  public Task(int id, String summary, String description, boolean isCompleted, Date createTime,
-      int duration) {
-    this.id = id;
-    this.summary = summary;
-    this.description = description;
-    this.isCompleted = isCompleted;
-    this.createTime = createTime;
-    this.duration = duration;
-
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getSummary() {
-    return summary;
-  }
-
-  public void setSummary(String summary) {
-    this.summary = summary;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public boolean isCompleted() {
-    return isCompleted;
-  }
-
-  public void setCompleted(boolean completed) {
-    isCompleted = completed;
-  }
-
-  public Date getCreateTime() {
-    return createTime;
-  }
-
-  public void setCreateTime(Date createTime) {
-    this.createTime = createTime;
-  }
-
-  public int getDuration() {
-    return duration;
-  }
-
-  public void setDuration(int duration) {
-    this.duration = duration;
-  }
-
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
+public class Task implements Serializable {
+    private static final long serialVersionUID = -3009157732242241606L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "task_id")
+    private long id;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "task_status")
+    private String taskStatus;
+    @Column(name = "create_date")
+    private Date createDate;
+    @Column(name = "process_time")
+    private Integer procressTime;
+    
+    
+    @ManyToMany
+    @JoinTable(name = "users_tasks", 
+        joinColumns = @JoinColumn(name = "task_id", referencedColumnName = "task_id"), 
+        inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "user_id"))
+    private Set<User> people = new HashSet<User>();
+    
+    public Task(String description, String taskStatus, Date createDate, Integer procressTime) {
+      super();
+      this.description = description;
+      this.taskStatus = taskStatus;
+      this.createDate = createDate;
+      this.procressTime = procressTime;
+    }
+    public String getDescription() {
+      return description;
+    }
+    public void setDescription(String description) {
+      this.description = description;
+    }
+    public String getTaskStatus() {
+      return taskStatus;
+    }
+    public void setTaskStatus(String taskStatus) {
+      this.taskStatus = taskStatus;
+    }
+    public Date getCreateDate() {
+      return createDate;
+    }
+    public void setCreateDate(Date createDate) {
+      this.createDate = createDate;
+    }
+    public Integer getProcressTime() {
+      return procressTime;
+    }
+    public void setProcressTime(Integer procressTime) {
+      this.procressTime = procressTime;
+    }
+    
 }
