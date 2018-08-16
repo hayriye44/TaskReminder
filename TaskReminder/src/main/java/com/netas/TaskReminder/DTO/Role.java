00fa889a60482029fa196e6e6a1a2e222e2026bc
Unknown <hayriye.96.44@gmail.com>
2018-08-16 14:09:17 +0300

    Update relationship

--- TaskReminder/src/main/java/com/netas/TaskReminder/DTO/Role.java
+++ TaskReminder/src/main/java/com/netas/TaskReminder/DTO/Role.java
@@ -1,5 +1,43 @@
 package com.netas.TaskReminder.DTO;
 
-public class Role {
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+  private static final long serialVersionUID = -3009157732242241606L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "role_id")
+  private long id;
+  @Column(name = "role_name")
+  private String roleName;
+
+
+  @OneToMany(mappedBy = "role")
+  private Set<User> users = new HashSet<User>();
+  //bir rolün birden fazla kullanıcı olabilir ama her kullanıcının bir rolü vardır.ya yöneticidir yada normal kullanıcıdır
+  
+  @ManyToMany(cascade = CascadeType.ALL)
+  @JsonBackReference
+  @JoinTable(name = "roles_authorizations",
+      joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "role_id"),
+      inverseJoinColumns = @JoinColumn(name = "authorization_id", referencedColumnName = "authorization_id"))
+  private Set<Authorization> authorizations = new HashSet<Authorization>();
+  //Bir Adminin birden fazla yetkisi olabilir hem ekleme hem silme yapabilir aynı şekilde bi yetki yi örneğin silme işlemini hem yönetici hem kullanıcı yapabilir
+  
 }
